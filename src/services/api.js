7b1ffeb4572dd4969b622719abc5f7b3b0e59eeb
Hitalo <hitalofrota@gmail.com>
2024-08-28 16:20:20 -0300

    modifications

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-//const API_URL = `https://1ad525f80dae235ce2c7a865ee12a211.serveo.net/api`;
 const API_URL = process.env.REACT_APP_API_URL;
 console.log('API_URL:', API_URL);
 
